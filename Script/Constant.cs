7d79a58b5dba367bbb1d24ee9a230bff0fe0c87c
ravimehtavc <113338474+ravimehtavc@users.noreply.github.com>
2023-05-01 18:39:33 +0530

    CG

---
+++ Script/Constant.cs
@@ -0,0 +1,3 @@
+public class Constant {
+    public static float rotateSpeed = 0.5f;
+}
--- Script/Rotate3dGameObject.cs
+++ Script/Rotate3dGameObject.cs
@@ -4,8 +4,6 @@ using EnhancedTouch = UnityEngine.InputSystem.EnhancedTouch;
 
 public class Rotate3dGameObject : MonoBehaviour {
     private NewInput touchControls;
-    public float rotatespeed = 10f;
-    private float _startingPosition;
 
     private void Awake() {
         touchControls = new NewInput();
@@ -35,31 +33,11 @@ public class Rotate3dGameObject : MonoBehaviour {
     }
 
     private void Update() {
-        //foreach (EnhancedTouch.Touch touch in EnhancedTouch.Touch.activeTouches) {          
-        //    if (touch.phase == UnityEngine.InputSystem.TouchPhase.Began) {
-        //        _startingPosition = touch.screenPosition.x;
-        //        Debug.Log("TouchPhase.Began");
-        //    } else if (touch.phase == UnityEngine.InputSystem.TouchPhase.Moved) {
-        //        //transform.Rotate(0f, touch.delta.x * 0.5f, 0f);
-        //        if (_startingPosition > touch.screenPosition.x) {
-        //            transform.Rotate(Vector3.up, -rotatespeed * Time.deltaTime);
-        //        } else if (_startingPosition < touch.screenPosition.x) {
-        //            transform.Rotate(Vector3.up, rotatespeed * Time.deltaTime);
-        //        }
-        //        Debug.Log("TouchPhase.Moved");
-        //    } else if (touch.phase == UnityEngine.InputSystem.TouchPhase.Ended) {
-        //        Debug.Log("TouchPhase.Ended");
-        //    } else if (touch.phase == UnityEngine.InputSystem.TouchPhase.Stationary) {
-        //        _startingPosition = touch.screenPosition.x;
-        //        Debug.Log("TouchPhase.Stationary");
-        //    }
-        //}
-
         foreach (EnhancedTouch.Touch touch in EnhancedTouch.Touch.activeTouches) {
             if (touch.phase == UnityEngine.InputSystem.TouchPhase.Began) {              
                 //Debug.Log("TouchPhase.Began");
             } else if (touch.phase == UnityEngine.InputSystem.TouchPhase.Moved) {
-                transform.Rotate(0f, touch.delta.x * 0.5f, 0f);               
+                transform.Rotate(0f, touch.delta.x * Constant.rotateSpeed, 0f);               
                 //Debug.Log("TouchPhase.Moved");
             } else if (touch.phase == UnityEngine.InputSystem.TouchPhase.Ended) {
                 Debug.Log("TouchPhase.Ended");
