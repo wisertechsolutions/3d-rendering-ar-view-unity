f2897c8b8536b57a2e7edcd7298224ef55edfd10
DivyaShah-VC <Divya.Shah@viitor.cloud>
2023-08-25 13:36:13 +0530

    Frame Logic Changed

--- Script/Manager/ThreeDARFrameCanvas.cs
+++ Script/Manager/ThreeDARFrameCanvas.cs
@@ -5,28 +5,16 @@ namespace ViitorCloud.ARModelViewer {
 
     public class ThreeDARFrameCanvas : MonoBehaviour {
         [SerializeField] private Image imageFrame;
-        [SerializeField] private Outline outline;
-
-        private static float divisionValueOutline = 20f; // We Found 100 unit => 5 then this is divisionValue
+        [SerializeField] private Image frameImage;
 
         public void DataToDisplay(Sprite imageToDisplay, Color frameColor) {
             imageFrame.sprite = imageToDisplay;
 
             FrameColorChange(frameColor);
-
-            if (imageToDisplay.texture.width > imageToDisplay.texture.height) {
-                outline.effectDistance = OutLineCalculations(imageToDisplay.texture.width);
-            } else {
-                outline.effectDistance = OutLineCalculations(imageToDisplay.texture.height);
-            }
         }
 
         public void FrameColorChange(Color frameColor) {
-            outline.effectColor = frameColor;
-        }
-
-        private Vector2 OutLineCalculations(float sizeImage) {
-            return new Vector2((sizeImage / divisionValueOutline), (sizeImage / divisionValueOutline));
+            frameImage.color = frameColor;
         }
     }
 }
